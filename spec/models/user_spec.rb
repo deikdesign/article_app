cc5a9bc9249e5aec0436a191e2158fb31c7e8692
deikdesign <deikdesign@gmail.com>
2021-05-06 23:59:43 +0300

    Add Rspec

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+  context 'tests' do
+    it 'presence' do
+      user = User.new(id: '', email: 'sample@example.com' ).save
+      expect(user).to eq(false)
+    end
+  end
+
+
+end
--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -1,12 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do 
-        pending "add some examples to (or delete) #{__FILE__}"
-
-        user = User.email(email: '')
-
-        expect(email).to eq ''
-        end
+    pending "add some examples to (or delete) #{__FILE__}"
+    pending "add some examples to (or delete) #{__FILE__}"
 end
-
-
